bf8497793b3cf1bf2832662dad5bad5743f809e7
walkerhilla <huny98.dev@gmail.com>
2023-09-28 00:42:09 +0900

    Localizable key 상수화 다국어 대응 key 문자열 리터럴을 상수화함

---
+++ DailyFootball/Resources/Constants/LocalizedStrings.swift
@@ -0,0 +1,43 @@
+//
+//  LocalizedStrings.swift
+//  DailyFootball
+//
+//  Created by walkerhilla on 2023/09/28.
+//
+
+import Foundation
+
+protocol Localizable {
+  var localizedValue: String { get }
+}
+
+extension Localizable where Self: RawRepresentable, Self.RawValue == String {
+  var localizedValue: String {
+    return NSLocalizedString(rawValue, comment: "")
+  }
+}
+
+enum LocalizedStrings {
+  enum Common: String, Localizable {
+    case ok = "common_ok"
+    case cancel = "common_cancel"
+  }
+  
+  enum TabBar {
+    enum Leagues: String, Localizable {
+      case title = "tab_leagues_title"
+    }
+    
+    enum Matches: String, Localizable {
+      case title = "tab_matches_title"
+    }
+    
+    enum Following: String, Localizable {
+      case title = "tab_following_title"
+    }
+    
+    enum News: String, Localizable {
+      case title = "tab_news_title"
+    }
+  }
+}
