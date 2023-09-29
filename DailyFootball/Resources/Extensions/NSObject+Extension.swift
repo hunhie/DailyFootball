e2cab60fcf3dbfe6070e5d36b0131f3d13197099
walkerhilla <huny98.dev@gmail.com>
2023-09-30 04:53:45 +0900

    NSObject idenfitier 확장 구현

---
+++ DailyFootball/Resources/Extensions/NSObject+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  NSObject+Extension.swift
+//  DailyFootball
+//
+//  Created by walkerhilla on 2023/09/29.
+//
+
+import Foundation
+
+extension NSObject {
+  static var identifier: String { Self.description() }
+}
