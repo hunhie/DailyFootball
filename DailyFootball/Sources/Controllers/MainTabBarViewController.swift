89095234da371f067abc043a1dbac97e8d69b184
walkerhilla <huny98.dev@gmail.com>
2023-10-04 09:37:38 +0900

    MainTabBar barTintColor 수정

--- DailyFootball/Sources/Controllers/MainTabBarViewController.swift
+++ DailyFootball/Sources/Controllers/MainTabBarViewController.swift
@@ -18,21 +18,23 @@ final class MainTabBarViewController: UITabBarController {
   func configureTabBar() {
     view.backgroundColor = .systemBackground
     
-    tabBar.barTintColor = .white
+    tabBar.barTintColor = .systemBackground
     tabBar.isTranslucent = false
     tabBar.tintColor = .label
     tabBar.unselectedItemTintColor = .gray
     
-    
     let league = UINavigationController(rootViewController: LeaguesViewController())
     league.tabBarItem.image = UIImage(systemName: "trophy.fill")
     league.tabBarItem.title = LocalizedStrings.TabBar.Leagues.title.localizedValue
+    
     let matches = UINavigationController(rootViewController: MatchesViewController())
     matches.tabBarItem.image = UIImage(systemName: "sportscourt.fill")
     matches.tabBarItem.title = LocalizedStrings.TabBar.Matches.title.localizedValue
+    
     let following = UINavigationController(rootViewController: FollowingViewController())
     following.tabBarItem.image = UIImage(systemName: "star.fill")
     following.tabBarItem.title = LocalizedStrings.TabBar.Following.title.localizedValue
+    
     let news = UINavigationController(rootViewController: NewsViewController())
     news.tabBarItem.image = UIImage(systemName: "newspaper.fill")
     news.tabBarItem.title = LocalizedStrings.TabBar.News.title.localizedValue
