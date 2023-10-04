ea10569cdd2f64c98623fb5b94f129ed218f1717
walkerhilla <huny98.dev@gmail.com>
2023-10-04 09:28:38 +0900

    APIFootball 통신 매니저 추가

---
+++ DailyFootball/Sources/API/APIServices/APIFootballManager.swift
@@ -0,0 +1,43 @@
+//
+//  APIFootballManager.swift
+//  DailyFootball
+//
+//  Created by walkerhilla on 2023/10/02.
+//
+
+import Foundation
+import Moya
+
+struct APIFootballManager {
+  
+  private let provider = MoyaProvider<APIFootballTarget>()
+
+  func request<Model: Decodable>(_ tager: APIFootballTarget, completion: @escaping (Result<Model, APIFootballError>) -> ()) {
+    provider.request(tager) { result in
+      switch result {
+      case .success(let response):
+        switch response.statusCode {
+        case 200:
+          do {
+            let decodedResponse = try JSONDecoder().decode(Model.self, from: response.data)
+            completion(.success(decodedResponse))
+          } catch let decodingError {
+            dump(decodingError)
+            completion(.failure(.decodingError))
+          }
+        case 204:
+          completion(.failure(.noData))
+        case 499:
+          completion(.failure(.timeout))
+        case 500:
+          completion(.failure(.serverError))
+        default:
+          completion(.failure(.unknown))
+        }
+      case .failure(let error):
+        dump(error)
+        completion(.failure(.unknown))
+      }
+    }
+  }
+}
