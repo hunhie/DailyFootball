3fd99a5345722a7a5a0d87edcb2a865d2bb8f50e
walkerhilla <huny98.dev@gmail.com>
2023-10-04 09:28:09 +0900

    APIFootball TargetType

---
+++ DailyFootball/Sources/API/APIServices/APIFootballTarget.swift
@@ -0,0 +1,41 @@
+//
+//  APIFootballTarget.swift
+//  DailyFootball
+//
+//  Created by walkerhilla on 2023/10/03.
+//
+
+import Foundation
+import Moya
+
+enum APIFootballTarget {
+  case leagues
+}
+
+extension APIFootballTarget: TargetType {
+  var path: String {
+    switch self {
+    case .leagues: return "leagues"
+    }
+  }
+  
+  var method: Moya.Method {
+    switch self {
+    case .leagues: return .get
+    }
+  }
+  
+  var task: Moya.Task {
+    switch self {
+    case .leagues: return .requestParameters(parameters: ["current": "true"], encoding: URLEncoding.queryString)
+    }
+  }
+  
+  var headers: [String : String]? {
+    ["X-RapidAPI-Key": APIKeys.apifootball.rawValue]
+  }
+  
+  var baseURL: URL {
+    URL(string: "https://api-football-v1.p.rapidapi.com/v3/")!
+  }
+}
