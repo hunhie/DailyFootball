080b2e046ccb8e48292e447797a99a06e031ce15
walkerhilla <huny98.dev@gmail.com>
2023-10-20 02:46:05 +0900

    TableView 편집 모드 델리게이트

---
+++ DailyFootball/Sources/Protocols/TableViewEditableDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  TableViewEditableDelegate.swift
+//  DailyFootball
+//
+//  Created by walkerhilla on 2023/10/16.
+//
+
+import Foundation
+
+protocol TableViewEditableDelegate: AnyObject {
+  func didTapEditButton(_ isEdit: Bool)
+}
+
