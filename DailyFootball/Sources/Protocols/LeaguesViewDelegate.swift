39822d5ffbd800d68e5fbec229a598a7583cccf9
walkerhilla <huny98.dev@gmail.com>
2023-10-19 21:38:49 +0900

    리그 뷰 액션 델리게이트 - 팔로우 / 언팔로우 - 국가 셀 펼치기 / 닫기

---
+++ DailyFootball/Sources/Protocols/LeaguesViewDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  LeaguesViewDelegate.swift
+//  DailyFootball
+//
+//  Created by walkerhilla on 2023/10/15.
+//
+
+import Foundation
+
+protocol LeaguesViewDelegate: AnyObject {
+  func didFollow(competition: Competition)
+  func didUnfollow(competition: Competition)
+  func didTapCompetitionGroup(competitionGroup: CompetitionGroup)
+}
