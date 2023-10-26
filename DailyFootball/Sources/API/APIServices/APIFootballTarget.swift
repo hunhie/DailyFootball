c2bf08dd9a3e880636b3b4fca71cc95f689fb266
walkerhilla <huny98.dev@gmail.com>
2023-10-26 18:09:59 +0900

    Standings, TopScorers Target 추가

--- DailyFootball/Sources/API/APIServices/APIFootballTarget.swift
+++ DailyFootball/Sources/API/APIServices/APIFootballTarget.swift
@@ -10,24 +10,36 @@ import Moya
 
 enum APIFootballTarget {
   case leagues
+  case standings(season: Int, id: Int)
+  case topScorers(season: Int, id: Int)
 }
 
 extension APIFootballTarget: TargetType {
   var path: String {
     switch self {
     case .leagues: return "leagues"
+    case .standings: return "standings"
+    case .topScorers: return "players/topscorers"
     }
   }
   
   var method: Moya.Method {
     switch self {
     case .leagues: return .get
+    case .standings: return .get
+    case .topScorers: return .get
     }
   }
   
   var task: Moya.Task {
     switch self {
     case .leagues: return .requestPlain
+    case .standings(let season, let id):
+      let parameters = ["season": "\(season)", "league": "\(id)"]
+      return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
+    case .topScorers(let season, let id):
+      let parameters = ["season": "\(season)", "league": "\(id)"]
+      return .requestParameters(parameters: parameters, encoding: URLEncoding.default)
     }
   }
   
