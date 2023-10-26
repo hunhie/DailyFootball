25b64f6f78633affedc4249e4891f8157b3f4158
walkerhilla <huny98.dev@gmail.com>
2023-10-26 18:14:50 +0900

    Border Style 추가

--- DailyFootball/Sources/Views/Components/StatefulButton.swift
+++ DailyFootball/Sources/Views/Components/StatefulButton.swift
@@ -11,11 +11,13 @@ class StatefulButton<T: Hashable>: UIButton {
   
   private var stateConfigurations: [T: UIButton.Configuration] = [:]
   private var stateBackgroundColors: [T: UIColor] = [:]
+  private var stateBorderColor: [T: UIColor] = [:]
   
   var currentState: T? {
     didSet {
       updateConfigurationForCurrentState()
       updateBackgroundColorForCurrentState()
+      updateBorder()
     }
   }
   
@@ -41,6 +43,10 @@ class StatefulButton<T: Hashable>: UIButton {
     stateBackgroundColors[state] = color
   }
   
+  func setBorder(_ color: UIColor, forState state: T) {
+    stateBorderColor[state] = color
+  }
+  
   private func updateConfigurationForCurrentState() {
     if let state = currentState, let configuration = stateConfigurations[state] {
       self.configuration = configuration
@@ -50,6 +56,15 @@ class StatefulButton<T: Hashable>: UIButton {
   private func updateBackgroundColorForCurrentState() {
     if let state = currentState, let backgroundColor = stateBackgroundColors[state] {
       self.backgroundColor = backgroundColor
+    }
+  }
+  
+  private func updateBorder() {
+    if let state = currentState, let borderColor = stateBorderColor[state] {
+      self.layer.borderColor = borderColor.cgColor
+      self.layer.borderWidth = 1
+    } else {
+      self.layer.borderWidth = 0
     }
   }
   
